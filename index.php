c0b447a6dd405febe5b9801df8b6e1b3c2115e12
Bruno <49537489+briuno@users.noreply.github.com>
2023-07-03 18:06:24 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Sua Landpage</title>
+  <link rel="stylesheet" type="text/css" href="CSS/estilo.css">
+</head>
+<body>
+  <header>
+    <h1>Sua Landpage</h1>
+    <nav>
+      <a href="#">Início</a>
+      <a href="#">Cursos</a>
+      <a href="#">Sobre</a>
+      <a href="#">Contato</a>
+    </nav>
+  </header>
+
+  <section class="hero-section">
+    <h2>Aprenda programação de forma interativa</h2>
+    <p>Explore nossos cursos e desenvolva suas habilidades de programação de maneira prática e divertida.</p>
+    <a href="#" class="cta-button">Ver Cursos</a>
+  </section>
+
+  <section class="feature-section">
+    <div class="feature-box">
+      <img src="icone-1.png" alt="Ícone 1">
+      <h3>Exercícios Práticos</h3>
+      <p>Aprenda programação com exercícios práticos e reais, fortalecendo seu conhecimento e habilidades.</p>
+    </div>
+    <div class="feature-box">
+      <img src="icone-2.png" alt="Ícone 2">
+      <h3>Suporte Personalizado</h3>
+      <p>Receba suporte personalizado de nossa equipe de instrutores experientes sempre que precisar.</p>
+    </div>
+    <div class="feature-box">
+      <img src="icone-3.png" alt="Ícone 3">
+      <h3>Ambiente de Aprendizado</h3>
+      <p>Desfrute de um ambiente de aprendizado colaborativo com fóruns de discussão e interação com outros alunos.</p>
+    </div>
+  </section>
+
+  <footer>
+    <p>&copy; 2023 Sua Landpage. Todos os direitos reservados.</p>
+  </footer>
+</body>
+</html>
